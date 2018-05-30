0a461443047e5a2fc7efb40641507aa494e13f99
iriszidon <iriszidon@gmail.com>
2018-05-30 15:44:37 +0300

    Add files via upload

---
+++ manage.py
@@ -0,0 +1,20 @@
+from flask_script import Manager, Server
+from application import create_app
+import os, sys
+
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+app = create_app()
+manager = Manager(app)
+
+# Turn on debugger by default and reloader
+manager.add_command("runserver", Server(
+    use_debugger = True,
+    use_reloader = True,
+    host = '0.0.0.0',
+    port = 5000
+))
+
+if __name__ == "__main__":
+    manager.run()
